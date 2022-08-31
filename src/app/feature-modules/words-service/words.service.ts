6534c9dbb6b86b7dead69e025fb1946712bfa9e9
Ayo <ramon.aycojr@gmail.com>
2022-08-31 15:07:54 +0200

    unique words only

--- src/app/feature-modules/words-service/words.service.ts
+++ src/app/feature-modules/words-service/words.service.ts
@@ -1,45 +1,46 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnDestroy } from '@angular/core';
 import { Word } from '../agent-board/agent-board.component';
-import {map, takeUntil} from 'rxjs/operators';
+import { map, takeUntil } from 'rxjs/operators';
 import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WordsService implements OnDestroy {
-
   subscriptions = new Subject<any>();
+  selected: string[] = [];
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnDestroy(): void {
-      this.subscriptions.next();
-      this.subscriptions.complete();
+    this.subscriptions.next();
+    this.subscriptions.complete();
   }
 
   getWords(length: number): Observable<Word[]> {
-     return this.http.get('/assets/words.txt', {responseType: 'text'})
-      .pipe(
-        takeUntil(this.subscriptions),
-        map(rawWords => {
-          const dictionary: string[] = rawWords.split(/\n/);
+    return this.http.get('/assets/words.txt', { responseType: 'text' }).pipe(
+      takeUntil(this.subscriptions),
+      map((rawWords) => {
+        const dictionary: string[] = rawWords.split(/\n/);
 
-          return Array.apply(null, Array(length)).map(() => {
-        return {
+        return Array.apply(null, Array(length)).map(() => {
+          return {
             text: this.getRandomWord(dictionary),
-            isSelected: false
-          }
-          });
-
-        })
-      );
-
+            isSelected: false,
+          };
+        });
+      })
+    );
   }
 
   getRandomWord(dictionary: string[]): string {
     const index = Math.floor(Math.random() * dictionary.length);
-    return dictionary[index];
+    let selectedWord = dictionary[index];
+    if (!this.selected.includes(selectedWord)) {
+      this.selected.push(selectedWord);
+      return selectedWord;
+    }
+    return this.getRandomWord(dictionary);
   }
 }
